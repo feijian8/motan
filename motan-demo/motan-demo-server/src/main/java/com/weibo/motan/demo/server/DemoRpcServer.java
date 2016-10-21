c23014766b26f23b9a424cf1d0e4a7969f47c4ed
feijian8 <feijian7716@126.com>
2016-10-21 14:26:06 +0800

    litaijun#chat#开始拍卖不需要pid

--- motan-demo/motan-demo-server/src/main/java/com/weibo/motan/demo/server/DemoRpcServer.java
+++ motan-demo/motan-demo-server/src/main/java/com/weibo/motan/demo/server/DemoRpcServer.java
@@ -23,10 +23,11 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class DemoRpcServer {
 
-    public static void main(String[] args) throws InterruptedException {
-        ApplicationContext applicationContext = new ClassPathXmlApplicationContext(new String[] {"classpath*:motan_demo_server.xml"});
-        MotanSwitcherUtil.setSwitcherValue(MotanConstants.REGISTRY_HEARTBEAT_SWITCHER, true);
-        System.out.println("server start...");
-    }
+	public static void main(String[] args) throws InterruptedException {
+		ApplicationContext applicationContext = new ClassPathXmlApplicationContext(
+				new String[] { "classpath*:motan_demo_server.xml" });
+		MotanSwitcherUtil.setSwitcherValue(MotanConstants.REGISTRY_HEARTBEAT_SWITCHER, true);
+		System.out.println("server start...");
+	}
 
 }
